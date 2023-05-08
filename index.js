8536f39711599e02e78ce248fd00c77ab833f729
Fernando Orduy <53240305+ferchorduy@users.noreply.github.com>
2023-05-08 15:47:57 -0400

    Add playRound function to play one single round and print result

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 //Make the computer play
 
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
 function getComputerChoice() {
 
     const choices = ['rock', 'paper', 'scissors'];
-
     return choices[Math.floor(Math.random() * 3)];
 
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = capitalize(playerSelection);
+    computerSelection = capitalize(computerSelection);
+
+    if ( playerSelection === 'rock' && computerSelection === 'scissors' || 
+         playerSelection === 'paper' && computerSelection === 'rock' || 
+         playerSelection === 'scissors' && computerSelection === 'paper' ) {
+            return `You win! ${playerSelection} beats ${computerSelection}.`;
+    }   else if ( playerSelection === computerSelection ) {
+            return `Tie! ${playerSelection} is equal to ${computerSelection}.`;
+    }   else {
+            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
