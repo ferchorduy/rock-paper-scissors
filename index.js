4abc18236453ce4883da2f99d495858b21096fe5
Fernando Orduy <53240305+ferchorduy@users.noreply.github.com>
2023-05-08 16:39:06 -0400

    Add game func and finish with messages to be displayed in game

--- index.js
+++ index.js
@@ -11,23 +11,50 @@ function getComputerChoice() {
 
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
 
     playerSelection = capitalize(playerSelection);
     computerSelection = capitalize(computerSelection);
 
-    if ( playerSelection === 'rock' && computerSelection === 'scissors' || 
-         playerSelection === 'paper' && computerSelection === 'rock' || 
-         playerSelection === 'scissors' && computerSelection === 'paper' ) {
+    if ( playerSelection === 'Rock' && computerSelection === 'Scissors' || 
+         playerSelection === 'Paper' && computerSelection === 'Rock' || 
+         playerSelection === 'Scissors' && computerSelection === 'Paper' ) {
+            playerScore += 1;
             return `You win! ${playerSelection} beats ${computerSelection}.`;
     }   else if ( playerSelection === computerSelection ) {
+            playerScore += 0.5;
+            computerScore += 0.5;
             return `Tie! ${playerSelection} is equal to ${computerSelection}.`;
     }   else {
+            computerScore += 1;
             return `You lose! ${computerSelection} beats ${playerSelection}.`;
     }
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+
+        const playerSelection = prompt("Rock, paper, or scissors?");
+        const computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`The score is You: ${playerScore} to Comp: ${computerScore}.`);
+
+    }
+
+    if ( playerScore > computerScore ) {
+        return `You win ${playerScore} to ${computerScore}!`
+    } else if (computerScore > playerScore) {
+        return `You lose ${playerScore} to ${computerScore}!`
+    } else {
+        return `You tie ${playerScore} to ${computerScore}!`
+    }
+
+}
+
+console.log(game());
